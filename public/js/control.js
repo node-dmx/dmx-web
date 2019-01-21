a3948012380f2c7bd3713f3e0508249723ff27d1
woverton <willovertonuk@gmail.com>
2019-01-21 21:59:56 +0000

    Fixed scene clearing

--- public/js/control.js
+++ public/js/control.js
@@ -2,8 +2,6 @@ const ShowController = function(app) {
 
 	$(".dmx-scene-trigger").on("click", (e) => {
 		app.socket.setScene($(e.target).attr("scene_id"))
-
-		console.log("dsfsfd")
 	})
 
 	return this
