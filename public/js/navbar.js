b61e5bec1910e7edceee74045e4f6aaf04a7e639
woverton <willovertonuk@gmail.com>
2019-02-08 23:51:35 +0000

    Stopped scrolling on simple sliders

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,6 +5,8 @@ $("#main-content-tabs .tab-pane").hide()
 $("#main-navbar .nav-link").click(function(e) {
   $("#main-content-tabs .tab-pane").hide()
   $($(this).attr("href")).show()
+
+  return false
 })
 
 /**
