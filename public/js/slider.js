bfc8a00dfd367e39f5c01e41c14990cd12bdb1ad
woverton <willovertonuk@gmail.com>
2019-02-10 14:54:26 +0000

    Can now save sliders as a new scene

--- public/js/slider.js
+++ public/js/slider.js
@@ -10,8 +10,47 @@ const SliderController = function(app) {
     app.socket.update(slider.attr("universe"), data)
   })
 
+  $("#sliders-scene-modal-save").on("click", (e) => {
+    const label = $("#sliders-scene-creator-modal-name").val()
+    $("#sliders-scene-creator-modal-name").val("")
+
+    this.saveScene(label)
+  })
+
   this.setSliderValue = (universe, channel, value) => {
     $(".dmx-channel[universe='" + universe + "'][channel='" + channel + "']").val(value)
+  }
+
+  this.saveScene = (label) => {
+    const id = app.socket.generateUUID()
+
+    const values = []
+
+    $(".dmx-channel").each((i, elem) => {
+      const value = Number($(elem).val())
+
+      if (value === 0) {
+        return
+      }
+
+      values.push({
+        type: "static",
+        channel: $(elem).attr("channel"),
+        value: value,
+        universe: $(elem).attr("universe")
+      })
+    })
+
+    scene = {
+      id: id,
+      label: label,
+      type: "full",
+      values: values
+    }
+
+    app.socket.saveScene(scene, (response) => {
+      window.location.reload(true)
+    })
   }
 
   return this
